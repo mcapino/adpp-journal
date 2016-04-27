ca74818e49f255c8e4a0cc73c48998ea3dfed881
Michal Cap <cap@agents.fel.cvut.cz>
2016-04-27 11:59:53 -0400

    updated the script to compile the experimental framework

--- run-experiment.sh
+++ run-experiment.sh
@@ -4,17 +4,32 @@
 CPUS=4
 
 # how many instances will be generated for each data point (i.e. for each number of robots in each instanceset)
-N=50
+N=2
 
 # list of environments
 ENVS="empty-hall-r22 empty-hall-r22-docks ubremen-r27 ubremen-r27-docks warehouse-r25 warehouse-r25-docks"
+
+echo "---------------------------------------------------------------------------------"
+echo " Compiling the implementation of algorithms, instance generator, simulator etc. "
+echo "---------------------------------------------------------------------------------"
+
+cd admap-solver
+mvn package
+cp target/admap-1.0-SNAPSHOT-jar-with-dependencies.jar ../solver.jar
+cd ..
+
+echo "----------------------------------------------------------"
+echo " Compilation finished! Will run the experiment now."
+echo "-----------------------------------------------------------"    
+
+# run the experiment for each environment
 
 for ENV in $ENVS
 do
     # cleanup
     rm -fr instances/$ENV
     mkdir -p instances/$ENV
-    
+
     echo "-------------------------------------------------------"
     echo " Generating instances for $ENV instanceset"
     echo "-------------------------------------------------------"
